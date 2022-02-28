4b5a5e801410ed75f5f119eedbb64286513b6208
Diabl0ke <95424831+Diabl0ke@users.noreply.github.com>
2022-02-28 15:13:03 +0100

    test

--- app.js
+++ app.js
@@ -6,27 +6,25 @@ const chargement = document.querySelector('.loader');
 
 const types = {
     grass: '#78c850',
-    ground: '#E2BF65',
-    dragon: '#6F35FC',
-    fire: '#F75231',
-    electric: '#F7D02C',
-    fairy: '#D685AD',
-    poison: '#966DA3',
-    bug: '#B3F594',
-    water: '#6390F0',
-    normal: '#D9D5D8',
-    psychic: '#F95587',
-    flying: '#A98FF3',
-    fighting: '#C25956',
+	ground: '#E2BF65',
+	dragon: '#6F35FC',
+	fire: '#F58271',
+	electric: '#F7D02C',
+	fairy: '#D685AD',
+	poison: '#966DA3',
+	bug: '#B3F594',
+	water: '#6390F0',
+	normal: '#D9D5D8',
+	psychic: '#F95587',
+	flying: '#A98FF3',
+	fighting: '#C25956',
     rock: '#B6A136',
     ghost: '#735797',
-    ice: '#96D9D6',
-    dark : '#735A4A',
-    steel :'#ADADC6'
+    ice: '#96D9D6'
 };
 
 
-function fetchPokemonBase() {
+function fetchPokemonBase(){
 
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         .then(reponse => reponse.json())
@@ -48,46 +46,46 @@ function fetchPokemonComplet(pokemon) {
     let nameP = pokemon.name;
 
     fetch(url)
+    .then(reponse => reponse.json())
+    .then((pokeData) => {
+        // console.log(pokeData);
+
+        objPokemonFull.pic = pokeData.sprites.front_default;
+        objPokemonFull.type = pokeData.types[0].type.name;
+        objPokemonFull.id = pokeData.id;
+
+
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
         .then(reponse => reponse.json())
         .then((pokeData) => {
             // console.log(pokeData);
 
-            objPokemonFull.pic = pokeData.sprites.front_default;
-            objPokemonFull.type = pokeData.types[0].type.name;
-            objPokemonFull.id = pokeData.id;
+            objPokemonFull.name = pokeData.names[4].name;
+            allPokemon.push(objPokemonFull);
 
+            if(allPokemon.length === 151) {
+                // console.log(allPokemon);
 
-            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
-                .then(reponse => reponse.json())
-                .then((pokeData) => {
-                    // console.log(pokeData);
+                tableauFin = allPokemon.sort((a,b) => {
+                    return a.id - b.id;
+                }).slice(0,21);
+                // console.log(tableauFin);
 
-                    objPokemonFull.name = pokeData.names[4].name;
-                    allPokemon.push(objPokemonFull);
-
-                    if (allPokemon.length === 151) {
-                        // console.log(allPokemon);
-
-                        tableauFin = allPokemon.sort((a, b) => {
-                            return a.id - b.id;
-                        }).slice(0, 21);
-                        // console.log(tableauFin);
-
-                        createCard(tableauFin);
-                        chargement.style.display = "none";
-                    }
-
-                })
+                createCard(tableauFin);
+                chargement.style.display = "none";
+            }
 
         })
+
+    })
 
 }
 
 // Création des cartes
 
-function createCard(arr) {
+function createCard(arr){
 
-    for (let i = 0; i < arr.length; i++) {
+    for(let i = 0; i < arr.length; i++) {
 
         const carte = document.createElement('li');
         let couleur = types[arr[i].type];
@@ -113,12 +111,12 @@ function createCard(arr) {
 
 window.addEventListener('scroll', () => {
 
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
     // scrollTop = scroll depuis le top
     // scrollHeight = scroll total
     // clientHeight = hauteur de la fenêtre, partie visible.
 
-    if (clientHeight + scrollTop >= scrollHeight - 20) {
+    if(clientHeight + scrollTop >= scrollHeight - 20) {
         addPoke(6);
     }
 
@@ -127,7 +125,7 @@ window.addEventListener('scroll', () => {
 let index = 21;
 
 function addPoke(nb) {
-    if (index > 151) {
+    if(index > 151) {
         return;
     }
     const arrToAdd = allPokemon.slice(index, index + nb);
@@ -140,15 +138,15 @@ function addPoke(nb) {
 
 // const formRecherche = document.querySelector('form');
 // formRecherche.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     recherche();
-// })
-
+    //     e.preventDefault();
+    //     recherche();
+    // })
+    
 searchInput.addEventListener('keyup', recherche);
 
-function recherche() {
+function recherche(){
 
-    if (index < 151) {
+    if(index < 151) {
         addPoke(130);
     }
 
@@ -156,13 +154,13 @@ function recherche() {
     filter = searchInput.value.toUpperCase();
     allLi = document.querySelectorAll('li');
     allTitles = document.querySelectorAll('li > h5');
-
-
-    for (i = 0; i < allLi.length; i++) {
+    
+    
+    for(i = 0; i < allLi.length; i++) {
 
         titleValue = allTitles[i].innerText;
 
-        if (titleValue.toUpperCase().indexOf(filter) > -1) {
+        if(titleValue.toUpperCase().indexOf(filter) > -1) {
             allLi[i].style.display = "flex";
         } else {
             allLi[i].style.display = "none";
@@ -181,9 +179,9 @@ function recherche() {
 
 // Animation Input
 
-searchInput.addEventListener('input', function (e) {
+searchInput.addEventListener('input', function(e) {
 
-    if (e.target.value !== "") {
+    if(e.target.value !== "") {
         e.target.parentNode.classList.add('active-input');
     } else if (e.target.value === "") {
         e.target.parentNode.classList.remove('active-input');
